6845ad5770fad1539ad302a9c212fab84b08b21b
JohnRickGD Official <168163669+JohnRickGDOfficial@users.noreply.github.com>
2024-08-29 19:59:25 +0800

    Update keepAlive.js

--- keepAlive.js
+++ keepAlive.js
@@ -1,13 +1,6 @@
-const http = require('http');
+var http = require('http');
 
-// Create an HTTP server to keep the bot alive
-const keepAlive = () => {
-    http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Bot is running');
-    }).listen(3000, () => {
-        console.log('Keep-alive server running on port 3000');
-    });
-};
-
-module.exports = keepAlive;
+http.createServer(function (req, res) {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
